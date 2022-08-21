485f741bb0e94d6e6c83cf4b714b42196bac8b46
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-08-21 00:19:01 -0500

    Fixed path variables in pkg.json and in all import statements, added import for dotenv

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,12 +1,12 @@
-import { cleanEnv, str, port } from "envalid";
+import { cleanEnv, str, port } from 'envalid';
 
 function validateEnv(): void {
   cleanEnv(process.env, {
     PORT: port({
-        default: 3001,
+      default: 5000,
     }),
     NODE_ENV: str({
-        choices: ["development", "production"]
+        choices: ['development', 'production']
     })
   });
 }
