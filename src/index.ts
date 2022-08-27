58d62032171efe6520791553d104fb7c283b0319
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-08-27 10:02:18 -0500

    Added auth middleware, added User controller to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import 'module-alias/register';
 import validateEnv from '@/utils/validateEnv';
 import App from './app';
 import PostController from '@/resources/post/post.controller';
+import UserController from '@/resources/user/user.controller';
 
 validateEnv();
 
-const app = new App([new PostController()], Number(process.env.PORT || 3001));
+const app = new App(
+    [new PostController(), new UserController()],
+    Number(process.env.PORT || 3001));
 
 app.listen();
